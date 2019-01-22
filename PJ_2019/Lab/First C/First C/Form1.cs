65811e4cc0c111ec094b097723c24a0c13706a49
svadakung <mumzaa@gmail.com>
2019-01-22 21:55:50 +0700

    test thai font

--- PJ_2019/Lab/First C/First C/Form1.cs
+++ PJ_2019/Lab/First C/First C/Form1.cs
@@ -19,7 +19,7 @@ namespace First_C
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            MessageBox.Show("สวัสดี");
         }
     }
 }
