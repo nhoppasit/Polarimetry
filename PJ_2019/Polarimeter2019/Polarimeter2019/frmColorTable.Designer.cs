ff9c6c1fc955c85c6abb4b42f89ba58d10aa5fc7
Peerakit <46627985+Peerakit@users.noreply.github.com>
2019-02-20 21:43:55 +0700

    Polarimeter2019 ครั้งที่ 1

---
+++ PJ_2019/Polarimeter2019/Polarimeter2019/frmColorTable.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Polarimeter2019
+{
+    partial class frmColorTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "1", System.Drawing.SystemColors.WindowText, System.Drawing.SystemColors.Window, new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "", System.Drawing.SystemColors.WindowText, System.Drawing.Color.Blue, new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "")}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("2");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("3");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("4");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("5");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("6");
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("7");
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("8");
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("9");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("10");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("11");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("12");
+            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("13");
+            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("14");
+            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("15");
+            System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem("16");
+            System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem("17");
+            System.Windows.Forms.ListViewItem listViewItem18 = new System.Windows.Forms.ListViewItem("18");
+            System.Windows.Forms.ListViewItem listViewItem19 = new System.Windows.Forms.ListViewItem("19");
+            System.Windows.Forms.ListViewItem listViewItem20 = new System.Windows.Forms.ListViewItem("20");
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lvColorTable = new System.Windows.Forms.ListView();
+            this.ColumnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(237, 300);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(115, 37);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lvColorTable
+            // 
+            this.lvColorTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvColorTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnHeader1,
+            this.ColumnHeader2,
+            this.ColumnHeader3});
+            this.lvColorTable.FullRowSelect = true;
+            this.lvColorTable.GridLines = true;
+            listViewItem1.UseItemStyleForSubItems = false;
+            this.lvColorTable.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8,
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12,
+            listViewItem13,
+            listViewItem14,
+            listViewItem15,
+            listViewItem16,
+            listViewItem17,
+            listViewItem18,
+            listViewItem19,
+            listViewItem20});
+            this.lvColorTable.Location = new System.Drawing.Point(13, 13);
+            this.lvColorTable.Margin = new System.Windows.Forms.Padding(4);
+            this.lvColorTable.Name = "lvColorTable";
+            this.lvColorTable.Size = new System.Drawing.Size(339, 279);
+            this.lvColorTable.TabIndex = 0;
+            this.lvColorTable.UseCompatibleStateImageBehavior = false;
+            this.lvColorTable.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnHeader1
+            // 
+            this.ColumnHeader1.Text = "No.";
+            this.ColumnHeader1.Width = 75;
+            // 
+            // ColumnHeader2
+            // 
+            this.ColumnHeader2.Text = "Color";
+            // 
+            // ColumnHeader3
+            // 
+            this.ColumnHeader3.Text = "Color Value";
+            this.ColumnHeader3.Width = 108;
+            // 
+            // frmColorTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 348);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lvColorTable);
+            this.Name = "frmColorTable";
+            this.Text = "frmColorTable";
+            this.Load += new System.EventHandler(this.frmColorTable_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnClose;
+        internal System.Windows.Forms.ListView lvColorTable;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader1;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader2;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader3;
+    }
+}
---
+++ PJ_2019/Polarimeter2019/Polarimeter2019/frmColorTable.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polarimeter2019
+{
+    public partial class frmColorTable : Form
+    {
+        public frmColorTable()
+        {
+            InitializeComponent();
+        }
+
+        private void frmColorTable_Load(object sender, EventArgs e)
+        {
+            LoadColorTable();
+        }
+
+        private void LoadColorTable()
+        {
+            lvColorTable.Items.Clear();
+            ListViewItem LVI = new ListViewItem();
+            LVI.UseItemStyleForSubItems = false;
+            LVI.Text = "Reference";
+            LVI.SubItems.Add("");
+            LVI.SubItems.Add(frmMain.ReferenceColor.ToString());
+            LVI.SubItems[1].BackColor = System.Drawing.Color.Red;
+            lvColorTable.Items.Add(LVI);
+
+            for (int i = 0; i <= 19; i++)
+            {
+                LVI = new ListViewItem();
+                LVI.UseItemStyleForSubItems = false;
+                LVI.Text = "Sample " + (i + 1).ToString();
+                LVI.SubItems.Add("");
+                LVI.SubItems.Add(frmMain.ColorTable(i).ToString());
+                LVI.SubItems[1].BackColor = frmMain.ColorTable(i);
+                lvColorTable.Items.Add(LVI);
+            }
+        }
+
+        private void lvColorTable_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                int i = lvColorTable.SelectedIndices[0];
+                ColorDialog dlg = new ColorDialog();
+                dlg.Color = lvColorTable.SelectedItems[0].SubItems[1].BackColor;
+                dlg.ShowDialog();
+                if (i == 0)
+                {
+                    frmMain.ReferenceColor = dlg.Color;
+                }
+                else
+                {
+                    frmMain.ColorTable[i - 1] = dlg.Color;
+                }
+
+                lvColorTable.SelectedItems[0].SubItems[1].BackColor = dlg.Color;
+                lvColorTable.SelectedItems[0].SubItems[2].Text = dlg.Color.ToString();
+            switch (i)
+            {
+                case 0:
+                    {
+                        My.Settings.ReferenceColor = dlg.Color;
+                        break;
+                    }
+
+                case 1:
+                    {
+                        My.Settings.Color1 = dlg.Color;
+                        break;
+                    }
+
+                case 2:
+                    {
+                        My.Settings.Color2 = dlg.Color;
+                        break;
+                    }
+
+                case 3:
+                    {
+                        My.Settings.Color3 = dlg.Color;
+                        break;
+                    }
+
+                case 4:
+                    {
+                        My.Settings.Color4 = dlg.Color;
+                        break;
+                    }
+
+                case 5:
+                    {
+                        My.Settings.Color5 = dlg.Color;
+                        break;
+                    }
+
+                case 6:
+                    {
+                        My.Settings.Color6 = dlg.Color;
+                        break;
+                    }
+
+                case 7:
+                    {
+                        My.Settings.Color7 = dlg.Color;
+                        break;
+                    }
+
+                case 8:
+                    {
+                        My.Settings.Color8 = dlg.Color;
+                        break;
+                    }
+
+                case 9:
+                    {
+                        My.Settings.Color9 = dlg.Color;
+                        break;
+                    }
+
+                case 10:
+                    {
+                        My.Settings.Color10 = dlg.Color;
+                        break;
+                    }
+
+                case 11:
+                    {
+                        My.Settings.Color11 = dlg.Color;
+                        break;
+                    }
+
+                case 12:
+                    {
+                        My.Settings.Color12 = dlg.Color;
+                        break;
+                    }
+
+                case 13:
+                    {
+                        My.Settings.Color13 = dlg.Color;
+                        break;
+                    }
+
+                case 14:
+                    {
+                        My.Settings.Color14 = dlg.Color;
+                        break;
+                    }
+
+                case 15:
+                    {
+                        My.Settings.Color15 = dlg.Color;
+                        break;
+                    }
+
+                case 16:
+                    {
+                        My.Settings.Color16 = dlg.Color;
+                        break;
+                    }
+
+                case 17:
+                    {
+                        My.Settings.Color17 = dlg.Color;
+                        break;
+                    }
+
+                case 18:
+                    {
+                        My.Settings.Color18 = dlg.Color;
+                        break;
+                    }
+
+                case 19:
+                    {
+                        My.Settings.Color19 = dlg.Color;
+                        break;
+                    }
+
+                case 20:
+                    {
+                        My.Settings.Color20 = dlg.Color;
+                        break;
+                    }
+            }
+            My.Settings.Save();
+            frmMain.ApplyColorTableToSamples();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
