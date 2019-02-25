5c52f3f1377d3ec3ff4d3419770dd349feded6b4
svadakung <mumzaa@gmail.com>
2019-02-26 00:42:14 +0700

    Copy to new location

---
+++ PJ_2019/Lab/Study Graph 02/Project4/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.txtBox5 = new System.Windows.Forms.TextBox();
+            this.txtBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.LsvData = new System.Windows.Forms.ListView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please choose your functions.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 33);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(348, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "------------------------------------------------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(28, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Palameter";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(146, 197);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(196, 31);
+            this.txtBox1.TabIndex = 4;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(146, 246);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(196, 31);
+            this.txtBox2.TabIndex = 5;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(146, 293);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(196, 31);
+            this.txtBox3.TabIndex = 6;
+            // 
+            // txtBox4
+            // 
+            this.txtBox4.Location = new System.Drawing.Point(146, 340);
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.Size = new System.Drawing.Size(196, 31);
+            this.txtBox4.TabIndex = 7;
+            // 
+            // txtBox5
+            // 
+            this.txtBox5.Location = new System.Drawing.Point(146, 390);
+            this.txtBox5.Name = "txtBox5";
+            this.txtBox5.Size = new System.Drawing.Size(196, 31);
+            this.txtBox5.TabIndex = 8;
+            // 
+            // txtBox6
+            // 
+            this.txtBox6.Location = new System.Drawing.Point(146, 438);
+            this.txtBox6.Name = "txtBox6";
+            this.txtBox6.Size = new System.Drawing.Size(196, 31);
+            this.txtBox6.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "A :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "B :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(96, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "W :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(103, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "T :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(55, 390);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Phase :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 438);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 25);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Number :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 481);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(348, 25);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "------------------------------------------------";
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(82, 520);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(200, 50);
+            this.btnCal.TabIndex = 17;
+            this.btnCal.Text = "CALCULATE";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(82, 576);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(200, 50);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(82, 632);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(200, 50);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // LsvData
+            // 
+            this.LsvData.Location = new System.Drawing.Point(366, 20);
+            this.LsvData.Name = "LsvData";
+            this.LsvData.Size = new System.Drawing.Size(596, 663);
+            this.LsvData.TabIndex = 18;
+            this.LsvData.UseCompatibleStateImageBehavior = false;
+            this.LsvData.SelectedIndexChanged += new System.EventHandler(this.LsvData_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(968, 20);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(900, 663);
+            this.chart1.TabIndex = 19;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1880, 695);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.LsvData);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBox6);
+            this.Controls.Add(this.txtBox5);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.TextBox txtBox5;
+        private System.Windows.Forms.TextBox txtBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListView LsvData;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PJ_2019/Lab/Study Graph 02/Project4/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        public int num = 0;
+        int tickIdx = 0;
+        double delta = 0.1;
+        Random rand = new Random();
+        private object lsvdata;
+        private int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LsvData.Columns.Add("n", 35);
+            LsvData.Columns.Add("t(n)", 150);
+            LsvData.Columns.Add("y(n)", 150);
+            LsvData.View = View.Details;
+            LsvData.GridLines = true;
+            LsvData.FullRowSelect = true;
+
+            comboBox1.Items.Add("y = A sin(wt + P) + B");
+            comboBox1.Items.Add("y = A cos(wt + P) + B");
+
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "0.00";
+            chart1.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "0.00";
+
+            timer1.Stop();
+            timer1.Enabled = false;
+            timer1.Interval = 1000;
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            Calculate();
+            StartNew();
+        }
+
+        public void Calculate()
+        {
+            switch (comboBox1.Text)
+            {
+                case "y = A sin(wt + P) + B":
+                    CalSin();
+                    break;
+
+                case "y = A cos(wt + P) + B":
+                    CalCos();
+                    break;
+            }
+        }
+
+        void CalSin()
+        {
+            try
+            {
+                int N = Convert.ToInt32(txtBox6.Text);
+
+                if (N <= i)
+                {
+                    timer1.Stop();
+                    return;
+                }
+
+                double A = Convert.ToDouble(txtBox1.Text);
+                double B = Convert.ToDouble(txtBox2.Text);
+                double w = Convert.ToDouble(txtBox3.Text);
+                double dT = Convert.ToDouble(txtBox4.Text);
+                double P = Convert.ToDouble(txtBox5.Text);
+                int Number = Convert.ToInt32(txtBox6.Text);
+
+                double[] tn = new double[N];
+                double[] yn = new double[N];
+                for (int i = 0; i < N; i++)
+                {
+                    tn[i] = i * dT;
+                    yn[i] = A * Math.Sin(w * (tn[i]) + P);
+
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = i.ToString();
+                    lvi.SubItems.Add(tn[i].ToString());
+                   // chart1.ChartAreas[0].AxisX.Minimum = tn[i];
+                   // chart1.ChartAreas[0].AxisX.Maximum = tn[i] + 1;
+                    lvi.SubItems.Add(yn[i].ToString());
+                   // chart1.ChartAreas[0].AxisX.Minimum = yn[i];
+                   // chart1.ChartAreas[0].AxisX.Maximum = yn[i] + 1;
+                    LsvData.Items.Add(lvi);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        void CalCos()
+        {
+            try
+            {
+                int N = Convert.ToInt32(txtBox6.Text);
+
+                if (N <= i)
+                {
+                    timer1.Stop();
+                    return;
+                }
+
+                double A = Convert.ToDouble(txtBox1.Text);
+                double B = Convert.ToDouble(txtBox2.Text);
+                double w = Convert.ToDouble(txtBox3.Text);
+                double dT = Convert.ToDouble(txtBox4.Text);
+                double P = Convert.ToDouble(txtBox5.Text);
+                int Number = Convert.ToInt32(txtBox6.Text);
+
+                double[] tn = new double[N];
+                double[] yn = new double[N];
+                for (int i = 1; i < N; i++)
+                {
+                    tn[i] = i * dT;
+                    yn[i] = A * Math.Cos(w * (tn[i]) + P);
+
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = i.ToString();
+                    lvi.SubItems.Add(tn[i].ToString());
+                    lvi.SubItems.Add(yn[i].ToString());
+                    LsvData.Items.Add(lvi);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "y = A sin(wt + P) + B":
+                    //CalSin();
+                    //PushXY();
+                    tickIdx++;
+                    break;
+
+                case "y = A cos(wt + P) + B":
+                    CalCos();
+                    //PushXY();
+                    //tickIdx++;
+                    break;
+                    i++;
+            }
+
+            PushXY();
+            tickIdx++;
+        }
+
+        void StartNew()
+        {
+            timer1.Stop();
+
+            tickIdx = 0;
+
+            // Reset number of series in the chart.
+            chart1.Series.Clear();
+
+            // create a line chart series
+            Series newSeries = new Series("Series1");
+            newSeries.ChartType = SeriesChartType.Line;
+            newSeries.BorderWidth = 2;
+            newSeries.XValueType = ChartValueType.DateTime;
+            chart1.Series.Add(newSeries);
+
+            chart1.ChartAreas[0].AxisX.Minimum = tickIdx * delta;
+            chart1.ChartAreas[0].AxisX.Maximum = (tickIdx + 1) * delta;
+
+            timer1.Start();
+        }
+
+        void PushXY()
+        {
+            foreach (Series ptSeries in chart1.Series)
+            {
+                double x = tickIdx * delta;
+                double y = rand.Next(10, 20);
+                System.Diagnostics.Trace.WriteLine(string.Format(">>> (X, Y) = ({0}, {1})", x, y));
+
+                ptSeries.Points.AddXY(x, y);
+                chart1.ChartAreas[0].AxisX.Maximum = ptSeries.Points[ptSeries.Points.Count - 1].XValue;
+                chart1.Invalidate();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+           LsvData.Items.Clear();
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void LsvData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
